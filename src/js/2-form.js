43816bbe695262309f73269d9cb9e3c608eb5fbc
a.viazovskyi <sentynel2014@gmail.com>
2026-04-16 01:07:07 +0200

    Homework completed according to tasks

---
+++ src/js/2-form.js
@@ -0,0 +1,38 @@
+let formData = { email: '', message: '' }
+
+const form = document.querySelector('.feedback-form')
+
+const email = form.querySelector('input')
+const textArea = form.querySelector('textarea')
+
+try {
+  const savedData = localStorage.getItem('feedback-form-state')
+
+  if (savedData) {
+    formData = JSON.parse(savedData)
+  }
+  email.value = formData.email || ''
+  textArea.value = formData.message || ''
+} catch (error) {
+  console.log(error.message)
+}
+
+form.addEventListener('input', handleInput)
+form.addEventListener('submit', handleSubmit)
+
+function handleInput(event) {
+  const { name, value } = event.target
+  formData[name] = value
+  localStorage.setItem('feedback-form-state', JSON.stringify(formData))
+}
+
+function handleSubmit(event) {
+  event.preventDefault()
+  if (!email.value.trim() || !textArea.value.trim()) {
+    alert('Fill please all fields')
+    return
+  }
+  formData = { email: '', message: '' }
+  localStorage.removeItem('feedback-form-state')
+  form.reset()
+}
